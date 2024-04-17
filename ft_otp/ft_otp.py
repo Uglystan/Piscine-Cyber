52867ac88f485d867c201eb1fffa4236ec011c4f
Lucas Girault <117684814+Uglystan@users.noreply.github.com>
2024-04-17 17:21:24 +0200

    Add files via upload

--- ft_otp/ft_otp.py
+++ ft_otp/ft_otp.py
@@ -1,14 +1,25 @@
 import os
 import sys
 import re
-import pyotp
 import time
-from cryptography.fernet import Fernet
+import hashlib
 
-def encode_key(key):
+def gen_otp(key):
+        t = int(time.time() // 10) #Retourne la division entiere entre le temps et 30 donc le resultat change toutes les 30sec
+        time_counter_bytes = t.to_bytes(t.bit_length(), 'big') #Convertir le nombre en binaire en big endian
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(key.encode())
+        sha256_hash.update(time_counter_bytes) #On encode la key avec le temps donc pendant 30sec c'est tjr le meme temps donc le m^ code
+        hmac_digest = sha256_hash.digest()
+        offset = int(t % 8) # Genere un nombre pseudo aleatoire entre 0 et 15
+        code = int.from_bytes(hmac_digest[offset:offset + 8], 'big') # On selectionne une plage de bytes en focntion de offset sur le hash et on le transofrme en int
+        print("totp:", code % 1000000) #On affiche les 6 derniers chiffre du code
+
+def store_hash_key(key):
         f = open("ft_otp.key", "w")
-        key.replace("1", "3")
-        print(key)
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(key.encode())
+        f.write(sha256_hash.hexdigest())
 
 def check_arg(arguments, key):
         if "-g" in arguments and len(arguments) == 3:
@@ -19,13 +30,18 @@ def check_arg(arguments, key):
                         key = arguments[arguments.index("-g") + 1]
                 if len(key) >= 64 and re.match(r"^[0-9a-fA-F]+$", key) != None:
                         try:
-                                encode_key(key)
+                                store_hash_key(key)
                         except Exception as e:
                                 print(e)
                 else:
                         print("Invalid key")
         elif "-k" in arguments and len(arguments) == 3:
-                print("K")
+                if os.path.exists(arguments[arguments.index("-k") + 1]) == True and os.access(arguments[arguments.index("-k") + 1], os.R_OK) == True:
+                        f = open(arguments[arguments.index("-k") + 1])
+                        key = f.read()
+                else:
+                        key = arguments[arguments.index("-k") + 1]
+                gen_otp(key)
         else:
                 print("Bad using")
 
